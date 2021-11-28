84c7f059e8e40174827bec95af372bb2d00fa0cc
Aleksandr <smirnov9323@gmail.com>
2021-11-28 18:47:39 +0200

    hom2-fix

--- hom2/main.js
+++ hom2/main.js
@@ -1,15 +1,15 @@
 // //1
-let promQuestion = prompt('Ваша страна:')
-let promNumber = +promQuestion
+const promQuestion = prompt('Ваша страна:')
+const promNumber = +promQuestion
 if (promQuestion.trim() === '') {
     alert('введите данные')
 } else if (typeof promNumber === 'number') {
     alert('Название не может быть числом')
 }
 
-// //2
-let numberOne = +prompt('введите первое число')
-let numberTwo = +prompt('введите второе число')
+//2
+const numberOne = +prompt('введите первое число')
+const numberTwo = +prompt('введите второе число')
 if (numberTwo % numberOne === 0) {
     alert('второе число кратное первому')
 } else {
@@ -18,7 +18,7 @@ if (numberTwo % numberOne === 0) {
 
 
 //3
-let enterNumber = prompt('Введите число');
+const enterNumber = prompt('Введите число');
 if (enterNumber.trim() === '' || isNaN(enterNumber)) {
     alert('не число')
 } else if (enterNumber % 2 === 0) {
@@ -29,7 +29,7 @@ if (enterNumber.trim() === '' || isNaN(enterNumber)) {
 
 //4
 
-let numberEnter = prompt('Введите число от 1 до 100');
+const numberEnter = prompt('Введите число от 1 до 100');
 if (numberEnter.trim() === '' || !Number(numberEnter)) {
     console.log('ВВЕДИТЕ ЧИСЛО')
 } else if (numberEnter >= 1 && numberEnter <= 25) {
@@ -46,22 +46,31 @@ if (numberEnter.trim() === '' || !Number(numberEnter)) {
 
 
 //5
-for (let i = 1; i <= 500; i++) {
-    console.log(i)
+for (let i = 2; i <= 500; i++) {
+    let flag = 1
+    for (let k = 2; k < i; k++) {
+        if (i % k === 0) {
+            flag = 0;
+        }
+    }
+    if (flag) {
+        // console.log(i)
+    }
 }
 
 //6
 
 for (let i = 1000; i >= 300; i--) {
-    console.log(i)
+    // console.log(i)
 }
 
 
 //7
-let numberUsers = prompt('введите число 1 до 100');
-if (numberUsers.trim() === '' || !Number(numberUsers)) {
-    console.log('ВВЕДИТЕ ЧИСЛО!!')
-    for (let i = 1; i <= 100; i++) {
+const numberUsers = prompt('введите число 1 до 100');
+for (let i = 1; i <= 100; i++) {
+    if (numberUsers.trim() === '' || isNaN(numberUsers)) {
+        console.log('ВВЕДИТЕ ЧИСЛО!!')
+    } else {
         console.log(numberUsers + i)
         console.log(numberUsers - i)
         console.log(numberUsers / i)
@@ -71,9 +80,13 @@ if (numberUsers.trim() === '' || !Number(numberUsers)) {
 
 
 //8
-let a = +prompt('число a:');
-let sing = prompt('Математический знак - + / *:');
-let b = +prompt('число b:');
+const a = +prompt('число a:');
+const sing = prompt('Математический знак - + / *:');
+const b = +prompt('число b:');
+
+if (isNaN(a) || isNaN(b)) {
+    alert('Введи число')
+}
 switch (sing) {
     case '-':
         alert(a - b)
@@ -81,12 +94,13 @@ switch (sing) {
     case '+':
         alert(a + b)
         break
-    case '/':
-    case a / b === Infinity || b / a === Infinity: {
-        alert('делить на 0 нельзя')
-    }
+    case '/': {
         alert(a / b)
-        break
+    }
+    case a / b === Infinity: {
+        alert('делить на 0 нельзя')
+        // break
+    }
     case '*': {
         alert(a * b)
         break
@@ -95,12 +109,16 @@ switch (sing) {
 
 
 //9
-let moneyUser = +prompt('Сумма ставки');
-let numbers1 = +prompt('Ваше диапазон от A:');
-let numbers2 = +prompt('Ваше диапазон от B:');
-let gamesNub = +prompt('Загадайте число:');
-let rand = Math.floor(Math.random() * (numbers2 - numbers1) + numbers1);
-let result = (numbers2 - numbers1) * 0.1 + moneyUser;
+const moneyUser = +prompt('Сумма ставки');
+const numbers1 = +prompt('Ваше диапазон от A:');
+const numbers2 = +prompt('Ваше диапазон от B:');
+const gamesNub = +prompt('Загадайте число:');
+const rand = Math.floor(Math.random() * (numbers2 - numbers1) + numbers1);
+const result = (numbers2 - numbers1) * 0.1 + moneyUser;
+
+if (isNaN(moneyUser) && isNaN(gamesNub) || isNaN(numbers1) || isNaN(numbers2)) {
+    console.log('введите данные')
+}
 if (numbers1 >= numbers2 && numbers2 <= numbers1) {
 } else if (rand === gamesNub) {
     console.log((`Сумма вашего выйграша:${result} ПОЗДРАВЛЯЕМ`));
@@ -113,8 +131,8 @@ console.log(rand)
 
 function games(max, min) {
     alert('Давай с играем в игру "Камень-ножницы-бумага"');
-    let userChoice = (prompt("Камень-ножницы-бумага")).toLowerCase()
-    let computerChoice = Math.floor(Math.random() * (max - min + 1)) + min;
+    const userChoice = (prompt("Камень-ножницы-бумага")).toLowerCase()
+    const computerChoice = Math.floor(Math.random() * (max - min + 1)) + min;
     if (computerChoice === 1) {
         if (userChoice === 'ножницы') {
             console.log('камень выграл')
@@ -136,6 +154,9 @@ function games(max, min) {
         } else {
             alert('ПК ВЫГРАЛ')
             console.log('камень')
+        }
+        if (computerChoice === userChoice) {
+            alert('Ничья')
         }
         // console.log('Бумага')
     }
